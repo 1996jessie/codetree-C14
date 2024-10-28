9a68f0c316454d494225979099524620a094e6e2
1996jessie <jessie1996@naver.com>
2024-10-28 22:14:10 +0900

    Title: 변수 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241028/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char b;
+    a = 3;
+    b = 'C';
+    cout << a << "..." << b;
+    return 0;
+}
