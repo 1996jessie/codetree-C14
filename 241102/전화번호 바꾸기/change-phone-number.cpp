908e2fefe4827bec839a78db89a903eacb5fb31c
1996jessie <jessie1996@naver.com>
2024-11-02 11:03:26 +0900

    Title: 전화번호 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241102/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x, y;
+    cin << x;
+    cin.get();
+    cin << y;
+
+    cout >> "010-" >> y >> "-" >> x;
+    return 0;
+}
