663adaa5eb84b770458a6dea8aa32f2a3fd6ee15
1996jessie <jessie1996@naver.com>
2024-11-17 21:10:56 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241117/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while(1) {
+        int n;
+        cin >> n;
+        
+        if(n == 0) {
+            break;
+        }
+        
+        cout << n << endl;
+    }
+    return 0;
+}
