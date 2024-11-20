3af12a0c469f4ac8fca0cfca7d2d0b949fb0ccaa
1996jessie <jessie1996@naver.com>
2024-11-20 09:51:27 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241120/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    
+    for(int i = 0; i < 2 * n; i++) {
+        if(i % 2 == 1) {
+            for(int k = 0; k < 1 + i / 2; k++) {
+                cout << "* ";
+            }
+        }
+        else {
+            for(int k = 0; k < n - i / 2; k++) {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
