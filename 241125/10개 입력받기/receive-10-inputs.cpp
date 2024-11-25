dae750dbefc258e4ec518e28a742aa1f7d604209
1996jessie <jessie1996@naver.com>
2024-11-25 10:47:44 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241125/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum = 0;
+    int cnt = 0;
+
+    for(int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if(arr[i] == 0) {
+            break;
+        }
+        sum += arr[i];
+        cnt++;
+    }
+
+    double avg = (double)sum / cnt;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << avg;
+
+    return 0;
+}
