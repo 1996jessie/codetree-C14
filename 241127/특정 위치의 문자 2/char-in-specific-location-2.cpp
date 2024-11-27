4b2b1d848b5c69d1fe3f980082b13e4fc60db68f
1996jessie <jessie1996@naver.com>
2024-11-27 10:38:35 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241127/특정 위치의 문자 2/char-in-specific-location-2.cpp
+++ 241127/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -5,8 +5,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     char arr[10];
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 10; i++) {
         cin >> arr[i];
+    }
 
     cout << arr[1] << " " << arr[4] << " " << arr[7];
 
