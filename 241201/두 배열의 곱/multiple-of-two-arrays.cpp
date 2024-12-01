3eaf28c7b6065c333cd887d1db5f189a6afee681
1996jessie <jessie1996@naver.com>
2024-12-01 21:14:28 +0900

    Title: 두 배열의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241201/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr_1[3][3];
+    int arr_2[3][3];
+    int arr_3[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr_1[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr_2[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            arr_3[i][j] = arr_1[i][j] * arr_2[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << arr_3[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
