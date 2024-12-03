904150c3ea97c7af0e3f91641d2d97d006efa997
1996jessie <jessie1996@naver.com>
2024-12-03 14:34:51 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241203/가장 짧은 문자열/shortest-string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1, str2, str3;
+    cin >> str1 >> str2 >> str3;
+
+    int len1 = str1.length();
+    int len2 = str2.length();
+    int len3 = str3.length();
+
+    int mx = len1;
+    if (mx < len2) { 
+        mx = len2; 
+    }
+    if (mx < len3) { 
+        mx = len3; 
+    }
+
+    int mn = len1;
+    if (mn > len2) { 
+        mn = len2; 
+    }
+    if (mn > len3) { 
+        mn = len3; 
+    }
+
+    cout << mx - mn;
+
+    return 0;
+}
