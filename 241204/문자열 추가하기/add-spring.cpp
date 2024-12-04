98eadb3909c701eb0b96d04cc9590855c4f89412
1996jessie <jessie1996@naver.com>
2024-12-04 17:56:31 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241204/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    string hel = "Hello";
+    
+    cin >> str;
+    str += hel;
+    cout << str;
+
+    return 0;
+}
