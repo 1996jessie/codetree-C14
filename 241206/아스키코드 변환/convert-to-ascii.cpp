c67e35408ff9ac0d48d2ef3638db53bc2e0a1aec
1996jessie <jessie1996@naver.com>
2024-12-06 09:07:36 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241206/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char x;
+	int a;
+	cin >> x >> a;
+	
+	cout << (int)x << " " << (char)a;
+	
+	return 0;
+}
