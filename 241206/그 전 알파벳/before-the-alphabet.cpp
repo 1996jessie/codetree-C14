f32f4697b28415ac2a2ba6c99462cfded312c762
1996jessie <jessie1996@naver.com>
2024-12-06 09:09:39 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241206/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char x;
+    cin >> x;
+
+    if (x == 'a') {
+        cout << "z";
+    } else {
+        cout << (char)(x - 1);
+    }
+
+    return 0;
+}
