f6262d5fc60954e9060e35877f51b0936973fc17
1996jessie <jessie1996@naver.com>
2024-12-09 11:47:08 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+
+bool Palindrome(string &s) {
+    for (int i = 0; i < (int) s.size(); i++) {
+        if (s[i] != s[(int) s.size() - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> str;
+
+    if (Palindrome(str)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+
+    return 0;
+}
