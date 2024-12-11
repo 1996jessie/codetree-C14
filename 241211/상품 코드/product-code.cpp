008e33a5e25b956bf08a0805b7b48731745ef82f
1996jessie <jessie1996@naver.com>
2024-12-11 18:36:28 +0900

    Title: 상품 코드, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241211/상품 코드/product-code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Product{
+    public:
+        string id;
+        int code;
+        Product(string id, int code) {
+            this->id = id;
+            this->code = code;
+        }
+};
+
+int main(){
+    // 여기에 코드를 작성해주세요.
+    Product product1 = Product("codetree", 50);
+    
+    string id2;
+    int code2;
+    cin >> id2 >> code2;
+    
+    Product product2 = Product(id2, code2);
+
+    cout << "product " << product1.code << " is " << product1.id << endl;
+    cout << "product " << product2.code << " is " << product2.id << endl;
+    return 0;
+}
