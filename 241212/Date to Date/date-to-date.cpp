c872a28694b4d5b84d0dde6f6e4b27d1e0922b44
1996jessie <jessie1996@naver.com>
2024-12-12 09:15:24 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241212/Date to Date/date-to-date.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int NumOfDays(int m, int d) {
+    int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+    
+    for(int i = 1; i < m; i++) {
+        total_days += days[i];
+    }
+    
+    total_days += d;
+    
+    return total_days;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1, m2, d1, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    
+    cout << NumOfDays(m2, d2) - NumOfDays(m1, d1) + 1;
+    return 0;
+}
