1807ed1414e2b56e055249c719117a6e856a9d9d
1996jessie <jessie1996@naver.com>
2024-12-12 09:01:52 +0900

    Title: 키를 기준으로 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241212/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#define MAXN 10
+
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        int height;
+        int weight;
+        Student(string name, int height, int weight) {
+            this->name = name;
+            this->height = height;
+            this->weight = weight;
+        }
+		Student(){}
+};
+
+bool Cmp(Student a, Student b) {
+    return a.height < b.height;
+}
+
+Student students[MAXN];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        string name;
+        int height, weight;
+        cin >> name >> height >> weight;
+        students[i] = Student(name, height, weight);
+    }
+
+    sort(students, students + n, Cmp);
+
+    for (int i = 0; i < n; i++){
+        cout << students[i].name << " ";
+        cout << students[i].height << " ";
+        cout << students[i].weight << endl;
+    }
+
+    return 0;
+}
