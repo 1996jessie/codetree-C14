183c8db7a185b21dcee71ec1d08a97b3810c796c
1996jessie <jessie1996@naver.com>
2024-12-15 20:51:31 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241215/모이자/gather.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <cstdlib>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int a[MAX_N];
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int ans = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        int sum_dist = 0;
+        for(int j = 0; j < n; j++) {
+            sum_dist += abs(j - i) * a[j];
+        }
+        ans = min(ans, sum_dist);
+    }
+
+    cout << ans;
+    return 0;
+}
