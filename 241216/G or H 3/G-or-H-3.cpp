ce6945a7f28ee75ba80f588744f77111ec202d19
1996jessie <jessie1996@naver.com>
2024-12-16 09:40:02 +0900

    Title: G or H 3, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 241216/G or H 3/G-or-H-3.cpp
+++ 241216/G or H 3/G-or-H-3.cpp
@@ -9,30 +9,30 @@ int n, k;
 int arr[MAX_NUM + 1];
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 입력
     cin >> n >> k;
-    for (int i = 0; i < n; i++) {
-        int x;
-        char c;
+    for(int i = 0; i < n; i++) {
+        int x; char c;
         cin >> x >> c;
         
-        if (c == 'G') {
+        if(c == 'G')
             arr[x] = 1;
-        } else {
+        else
             arr[x] = 2;
-        }
     }
-
+    
+    // 모든 구간의 시작점을 잡아봅니다.
     int max_sum = 0;
-    for (int i = 0; i <= MAX_NUM - k; i++) {
+    for(int i = 0; i <= MAX_NUM - k; i++) {
+        // 해당 구간 내 원소의 합을 구합니다.
         int sum = 0;
-        for (int j = i; j < i + k; j++) {
+        for(int j = i; j <= i + k; j++)
             sum += arr[j];
-        }
-
+        
+        // 최댓값을 구합니다.
         max_sum = max(max_sum, sum);
     }
-
+                        
     cout << max_sum;
     return 0;
 }
