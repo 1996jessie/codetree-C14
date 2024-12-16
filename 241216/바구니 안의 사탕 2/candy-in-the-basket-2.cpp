b83b2c776f4271e9a6cf8c11ac2fa4760dd5ba69
1996jessie <jessie1996@naver.com>
2024-12-16 09:45:39 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_NUM 100
+
+using namespace std;
+
+int n, k;
+int arr[MAX_NUM + 1];
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        int x, a;
+        cin >> a >> x;
+        
+        arr[x] += a;
+    }
+    
+    int max_sum = 0;
+    for(int i = 0; i <= MAX_NUM; i++) {
+        int sum = 0;
+        for(int j = i - k; j <= i + k; j++) {
+            if(j >= 0 && j <= MAX_NUM) {
+                sum += arr[j];
+            }
+        }
+        
+        max_sum = max(max_sum, sum);
+    }
+    
+    cout << max_sum;
+    return 0;
+}
