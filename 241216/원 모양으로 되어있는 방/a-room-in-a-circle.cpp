57b41c446a99d11f3cd130e77cd96dd8324bed42
1996jessie <jessie1996@naver.com>
2024-12-16 09:26:46 +0900

    Title: 원 모양으로 되어있는 방, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241216/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <cstdlib>
+
+#define MAX_N 1003
+
+using namespace std;
+
+int n;
+int a[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        int sum_dist = 0;
+        for (int j = 0; j < n; j++) {
+            int dist = (j + n - i) % n;
+            sum_dist += dist * a[j];
+        }
+
+        ans = min(ans, sum_dist);
+    }
+
+    cout << ans;
+    return 0;
+}
