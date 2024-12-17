e578bde8dd5573325694c4a158c7e4b01197c5e9
1996jessie <jessie1996@naver.com>
2024-12-17 10:32:03 +0900

    Title: 운행 되고 있는 시간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241217/운행 되고 있는 시간/hours-in-service.cpp
+++ 241217/운행 되고 있는 시간/hours-in-service.cpp
@@ -13,25 +13,31 @@ int main() {
     // 여기에 코드를 작성해주세요
     cin >> n;
     
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) {
         cin >> l[i] >> r[i];
+    }
     
     int ans = 0;
 
     for(int i = 0; i < n; i++) {
         int count[MAX_NUM] = {};
-        
+
         for(int j = 0; j < n; j++) {
-            if(j == i) continue;
-            
-            for(int k = l[j]; k < r[j]; k++)
+            if(j == i) {
+                continue;
+            }
+
+            for(int k = l[j]; k < r[j]; k++) {
                 count[k]++;
+            }
         }
         
         int time = 0;
         
         for(int j = 1; j < MAX_NUM; j++) {
-            if(count[j]) time++;
+            if(count[j]) {
+                time++;
+            }
         }
 
         ans = max(ans, time);
