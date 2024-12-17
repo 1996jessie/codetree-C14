305380f3cafffaf69d3fbdda135c7db6e7a07ba0
1996jessie <jessie1996@naver.com>
2024-12-17 10:33:37 +0900

    Title: 스승의 은혜 2, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241217/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n, b;
+int p[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> n >> b;
+
+    for(int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+
+    int ans = 0;
+    
+    for(int i = 0; i < n; i++) {
+        int tmp[MAX_N] = {};
+        for(int j = 0; j < n; j++) {
+            tmp[j] = p[j];
+        }
+        tmp[i] /= 2;
+
+        sort(tmp, tmp + n);
+
+        int student = 0;
+        int cnt = 0;
+
+        for(int j = 0; j < n; j++) {
+            if(cnt + tmp[j] > b) {
+                break;
+            }
+            cnt += tmp[j];
+            student++;
+        }
+
+        ans = max(ans, student);
+    }
+
+    cout << ans;
+    
+    return 0;
+}
