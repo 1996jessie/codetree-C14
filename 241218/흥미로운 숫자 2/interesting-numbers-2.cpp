a21e199cb83c71ed60100797bd478bb4c05eb126
1996jessie <jessie1996@naver.com>
2024-12-18 10:14:36 +0900

    Title: 흥미로운 숫자 2, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241218/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int x, y;
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> x >> y;
+
+    int ans = 0;
+
+    for(int i = x; i <= y; i++) {
+        int num = i;
+        int digit[10] = {};
+        int all_digits = 0;
+        while(num) {
+            digit[num % 10]++;
+            all_digits++;
+            num /= 10;
+        }
+
+        bool interesting = false;
+
+        for(int j = 0; j < 10; j++) {
+            if(digit[j] == all_digits - 1) {
+                interesting = true;
+            }
+        }
+
+        if(interesting) {
+            ans++;
+        }
+    }
+    
+    cout << ans;
+    return 0;
+}
