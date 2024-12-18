4838cb19adfdedd203efa7de9ef99f68fb2b214e
1996jessie <jessie1996@naver.com>
2024-12-18 10:20:55 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241218/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <climits>
+
+using namespace std;
+
+#define MAX_N 100
+
+int n;
+int arr[MAX_N];
+
+int Score(int removed_idx) {
+    int sum = 0;
+    int prev = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(i == removed_idx) {
+            continue;
+        }
+        if(prev != -1) {
+            sum += abs(arr[i] - prev);
+        }
+        prev = arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    int min_score = INT_MAX;
+
+    for(int i = 0; i < n; i++) {
+        arr[i] *= 2;
+
+        for(int j = 0; j < n; j++) {
+            min_score = min(min_score, Score(j));
+        }
+
+        arr[i] /= 2;
+    }
+    printf("%d", min_score);
+    return 0;
+}
