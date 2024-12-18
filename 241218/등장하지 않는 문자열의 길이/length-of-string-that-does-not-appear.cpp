5197dd91dc4bb1e2548d0a46797d42468f10021c
1996jessie <jessie1996@naver.com>
2024-12-18 10:29:32 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241218/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> n;
+    cin >> str;
+
+    int ans = 1;
+
+    for(int i = 1; i < n; i++) {
+        bool twice = false;
+
+        for(int j = 0; j <= n - i; j++) {
+            for(int k = j + 1; k <= n - i; k++) {
+                bool issame = true;
+
+                for(int l = 0; l < i; l++) {
+                    if(str[j + l] != str[k + l]) {
+                        issame = false;
+                    }
+                }
+
+                if(issame) {
+                    twice = true;
+                }
+            }
+        }
+
+        if(twice) {
+            ans = i + 1;
+        } else {
+            break;
+        }
+    }
+
+    cout << ans;
+    
+    return 0;
+}
