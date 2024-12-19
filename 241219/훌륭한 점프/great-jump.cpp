12bbedc762a48541d06af418d9bc314476cca2a1
1996jessie <jessie1996@naver.com>
2024-12-19 19:51:54 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241219/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+#define MAX_NUM 100
+
+using namespace std;
+
+int n, k;
+int arr[MAX_N];
+
+bool IsPossible(int limit) {
+    int last_idx = 0;
+    for(int i = 1; i < n; i++) {
+        if(arr[i] <= limit) {
+            if(i - last_idx > k) {
+                return false;
+            }
+            last_idx = i;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(int i = max(arr[0], arr[n - 1]); i <= MAX_NUM; i++) {
+        if(IsPossible(i)) {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
