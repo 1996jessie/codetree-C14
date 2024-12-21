1ed7cdf8c5114bbcd48c8c157b48178c203e455e
1996jessie <jessie1996@naver.com>
2024-12-21 10:39:21 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241221/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int blocks[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> blocks[i];
+    }
+
+    int idx = n - 2;
+    while (idx >= 0 && blocks[idx] < blocks[idx + 1]) {
+        idx--;
+    }
+
+    cout << idx + 1;
+    return 0;
+}
