00cc87ecf6c44096dbceda0ccc8ff989c14b0f17
1996jessie <jessie1996@naver.com>
2024-12-21 10:45:54 +0900

    Title: 두 점으로 만드는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241221/두 점으로 만드는 정사각형/square-made-of-two-points.cpp
+++ 241221/두 점으로 만드는 정사각형/square-made-of-two-points.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <algorithm>
 
-#define MAX_N 15
-
-int n = 15;
-int arr[MAX_N];
-
 using namespace std;
+
+int x1, x2, y1, y2;
+int a1, a2, b1, b2;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    
-    sort(arr, arr + n);
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
 
-    int a = arr[0];
-    int b = arr[1];
-    int c = arr[2];
+    int width = max(x2, a2) - min(x1, a1);
+    int height = max(y2, b2) - min(y1, b1);
 
-    int d = arr[n - 1] - a - b - c;
-
-    cout << a << " " << b << " " << c << " " << d;
+    cout << max(width, height) * max(width, height);
     
     return 0;
 }
