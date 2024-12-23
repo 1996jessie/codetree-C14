cd9601ef8cf571a9d6ab57b969aa9732919e4405
1996jessie <jessie1996@naver.com>
2024-12-23 17:39:32 +0900

    Title: 정수 명령 처리 8, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241223/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    list<int> l;  
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+        if(command == "push_front") {
+            int x;
+            cin >> x;
+            l.push_front(x);
+        } else if(command == "push_back") {
+            int x;
+            cin >> x;
+            l.push_back(x);
+        } else if(command == "pop_front") {
+            int v = l.front();
+            l.pop_front();
+            cout << v << endl;
+        } else if(command == "pop_back") {
+            int v = l.back();
+            l.pop_back();
+            cout << v << endl;
+        } else if(command == "size") {
+            cout << l.size() << endl;
+        } else if(command == "empty") {
+            if(l.empty()) {
+                cout << 1 << endl;
+            } else {
+                cout << 0 << endl;
+            }
+        } else if(command == "front") {
+            cout << l.front() << endl;
+        } else {
+            cout << l.back() << endl;
+        }
+    }
+    return 0;
+}
